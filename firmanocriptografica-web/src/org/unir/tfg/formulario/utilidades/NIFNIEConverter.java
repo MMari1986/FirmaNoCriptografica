1f0859332ca5d012d5e456b484d4cf2e0062f343
mmagdalena.hernandez <mmagdalena.hernandez@ESL63LLYZ1.itc.com>
2019-09-28 00:23:30 +0200

    Eliminación de avisos

--- firmanocriptografica-web/src/org/unir/tfg/formulario/utilidades/NIFNIEConverter.java
+++ firmanocriptografica-web/src/org/unir/tfg/formulario/utilidades/NIFNIEConverter.java
@@ -10,7 +10,6 @@ import javax.faces.convert.ConverterException;
 import javax.faces.convert.FacesConverter;
 import javax.faces.validator.ValidatorException;
 
-@SuppressWarnings("rawtypes")
 @FacesConverter("NIFNIEConverter")
 public class NIFNIEConverter implements Converter {
 
